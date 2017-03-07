1de0ad0cc78d8364ec89d10dacf07667509aa16c
Minjia-Zhu <Minjia-Zhu@users.noreply.github.com>
2017-03-06 22:46:59 -0600

    reset Django ui

---
+++ ChicagoWanderlust/search/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+import datetime
+from django.forms.extras.widgets import SelectDateWidget
+from django.forms import ModelForm, Form
+
+
+ATTRACTIONS_OPTIONS = ['Architecture','Art & Culture','History','Kids/Family',
+					   'Landmarks & Sightseeing','Museums','Parks & Nature',
+					   'Sports & Entertainment']
+TRANS_OPTIONS = ['Drive', 'Public']
+
+def _dropdown_menu(options):
+	return [(x,x) for x in options]
+
+ATTRACTIONS = _dropdown_menu(ATTRACTIONS_OPTIONS)
+TRANS = _dropdown_menu(TRANS_OPTIONS)
+
+class RecommendationForm(forms.Form):
+	checkin = forms.DateField(
+		    widget=SelectDateWidget,
+		    label='Check-in Date',
+		    required=False)
+	checkout = forms.DateField(
+		    widget=SelectDateWidget,
+		    label='Check-out Date',
+		    required=False)
+	pricemin = forms.IntegerField(
+			label='Minimum Price',
+			help_text='e.g. 100',
+			required=False)
+	pricemax = forms.IntegerField(
+			label='Maximum Price',
+			help_text='e.g. 500',
+			required=False)
+	attraction = forms.MultipleChoiceField(
+		    label='Attractions Preferences',
+		    choices=ATTRACTIONS,
+		    widget=forms.CheckboxSelectMultiple,
+		    required=False)
+	trans = forms.ChoiceField(
+		    label='Transportation',
+		    choices=TRANS,
+		    required=True)
+
+	
+	
+
+
+
+
+
+
+
+
+
