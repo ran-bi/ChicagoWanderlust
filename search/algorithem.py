522ed984608770682bd4bd63511f412fe5c71ca6
ran-bi <rbi@uchicago.edu>
2017-03-06 20:26:36 -0600

    add algorithem2 for testing front-back linkage, algorithem back to original form

--- search/algorithem.py
+++ search/algorithem.py
@@ -1,46 +1,4 @@
-from . import booking_scraper
-
 def foof(data):
-	CHECKIN = data['checkin']
-	CHECKOUT = data['checkout']
-	PRICEMIN = data['pricemin']
-	PRICEMAX = data['pricemax']
-	AIRBNB = booking_scraper.booking(CHECKIN, CHECKOUT, PRICEMIN, PRICEMAX)
-	rv = AIRBNB.iloc[:5, :].to_dict()
-
-
 	criteris_met = True
-	context = {'checkin': rv['url'][1],
-	'checkout': rv['name'][2],
-	'pricemin': rv['url'][2]}
-
+	context = data
 	return (criteris_met,context)
-
-
-
-
-'''
-{'pricemax': 500, 
-'attraction': ['Architecture', 'Kids/Family'], 
-'checkin': datetime.date(2017, 4, 1), 
-'pricemin': 50, 
-'trans': 'Drive', 
-'checkout': datetime.date(2017, 4, 2)}
-'''
-
-
-'''	
-	CHECKIN = data['checkin']
-	CHECKOUT = data['checkout']
-	PRICEMIN = data['pricemin']
-	PRICEMAX = data['pricemax']
-	AIRBNB = airbnb(CHECKIN, CHECKOUT, PRICEMIN, PRICEMAX)
-	BOOKING = booking(CHECKIN, CHECKOUT, PRICEMIN, PRICEMAX)
-	LOCATIONS = AIRBNB.append(BOOKING, ignore_index=True).iloc[:5, :]
-	rv = LOCATIONS.to_dict
-	context = {'checkin': rv['url'][1],
-	'checkout': rv['name'][2],
-	'pricemin': rv['url'][2]}
-'''
-
-
