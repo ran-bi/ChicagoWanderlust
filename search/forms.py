af8322637e064b5d23c1c0c06d845f478311916e
Minjia-Zhu <Minjia-Zhu@users.noreply.github.com>
2017-03-08 16:28:48 -0600

    adjust attraction preference input format

--- search/forms.py
+++ search/forms.py
@@ -4,10 +4,10 @@ from django.forms.extras.widgets import SelectDateWidget
 from django.forms import ModelForm, Form
 
 
-ATTRACTIONS_OPTIONS = ['Architecture','Art & Culture','History','Kids/Family',
+ATTRACTIONS_OPTIONS = ['None', 'Architecture','Art & Culture','History','Kids/Family',
 					   'Landmarks & Sightseeing','Museums','Parks & Nature',
 					   'Sports & Entertainment']
-TRANS_OPTIONS = ['driving', 'walking', 'bicycling', 'transit']
+TRANS_OPTIONS = ['driving','transit']
 
 def _dropdown_menu(options):
 	return [(x,x) for x in options]
@@ -32,11 +32,17 @@ class RecommendationForm(forms.Form):
 			label='Maximum Price',
 			help_text='e.g. 500',
 			required=False)
-	attraction = forms.MultipleChoiceField(
-		    label='Attractions Preferences',
+	attraction_1 = forms.ChoiceField(
+		    label='Attraction 1st Preference',
 		    choices=ATTRACTIONS,
-		    widget=forms.CheckboxSelectMultiple,
-		    help_text='Maximum 3 choices',
+		    required=False)
+	attraction_2 = forms.ChoiceField(
+		    label='Attraction 2nd Preference',
+		    choices=ATTRACTIONS,
+		    required=False)
+	attraction_3 = forms.ChoiceField(
+		    label='Attraction 3rd Preference',
+		    choices=ATTRACTIONS,
 		    required=False)
 	trans = forms.ChoiceField(
 		    label='Transportation',
